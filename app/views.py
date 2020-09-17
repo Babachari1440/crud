4ef7130b6ebc89b89bdb29277ff7a7e3d51b10fd
Babachari Vuttaradi <babavchari@gmail.com>
2020-09-17 15:57:38 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,109 @@
+from django.shortcuts import render
+from app.models import *
+from django.http import HttpResponse
+# Create your views here.
+def topic(request):
+    if request.method=='POST':
+        topic=request.POST['topic_name']
+        t=Topic.objects.get_or_create(topic_name=topic)[0]
+        t.save()
+        return HttpResponse('The given data has added into to ur Topic model successfully')
+
+
+
+
+    return render(request,'topic.html')
+
+
+
+def webpage(request):
+    if request.method=="POST":
+        topic=request.POST.get('topic_name')
+        name=request.POST.get('name')
+        url=request.POST.get('url')
+        t=Topic.objects.get_or_create(topic_name=topic)[0]
+        t.save()
+        w=Webpage.objects.get_or_create(topic_name=t,name=name,url=url)[0]
+        w.save()
+        return HttpResponse('one record has addedd into Webpage Model successfully')
+
+    return render(request,'webpage.html',context={'topics':Topic.objects.all()})
+
+
+
+def displaytopicform(request):
+    if request.method=="POST":
+        topic=request.POST.get('topic_name')
+        webpages=Webpage.objects.filter(topic_name=topic)
+        #print(webpages)
+        return render(request,'displaywebpage.html',context={'webpages':webpages})
+
+    return render(request,'displaytopicform.html',context={'topics':Topic.objects.all()})
+
+def updateweb(request):
+    if request.method=='POST':
+        name=request.POST.get('name')
+        url=request.POST.get('url')
+        Webpage.objects.filter(name=name).update(url=url)
+
+        
+    return render(request,'updateweb.html')
+
+
+def access_records(request):
+    if request.method=='POST':
+       
+        
+        
+        name=request.POST.get('name')
+        url=request.POST.get('url')
+        date=request.POST.get('date')
+        w=Webpage.objects.get_or_create(name=name,url=url)[0]
+        w.save()
+
+        a=Access_Records.objects.get_or_create(name=name,date=date)[0]
+        a.save()
+
+        return HttpResponse('one record has addedd into Access_Records Model successfully')
+
+
+        return render(request,'access_records.html',context={'webpages':Webpage.objects.all()})
+def display_access_records(request):
+    if request.method=='POST':
+        webpage=request.POST.get('name')
+        topic=request.POST.get('topic_name')
+        webpages=Webpage.objects.filter(topic_name=topic,name=name)[0]
+        access_records=Access_Records.objects.filter(name=name)
+        #print(webpages)
+    return render(request,'displaywebpage.html',context={'webpages':webpages})
+
+    return render(request,'display_access_records.html',context={'topics':Topic.objects.all()})
+def select(request):
+    return render(request,'select.html')
+
+
+def delete_topic(request):
+    if request.method=='POST':
+        topic=request.POST['topic_name']
+        Topic.objects.filter(topic_name=topic).delete()
+        return HttpResponse('one topic has deleted successfully')
+
+def multi_select(request):
+    if request.method=='POST':
+        topics=request.POST.getlist('topic_name')
+        #print(topics)
+        webpages=Webpage.objects.none()
+        for i in topics:
+            webpages=webpages | Webpage.objects.filter(topic_name=i)
+
+        return render(request,'displaywebpage.html',context={'webpages':webpages})
+    return render(request,'multi_select.html',context={'topics':Topic.objects.all()})
+
+
+
+def checkbox(request):
+    return render(request,'checkbox.html',context={'topics':Topic.objects.all()})
+
+
+
+
---
+++ project18/urls.py
@@ -0,0 +1,38 @@
+"""project18 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from app import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('topic/',views.topic,name='topic'),
+    path('webpage/',views.webpage,name='webpage'),
+    path('displaytopicform/',views.displaytopicform,name='displaytopicform'),
+    path('updateweb/',views.updateweb,name='updateweb'),
+    path('access_records/',views.access_records,name='access_records'),
+    path('display_access_records/',views.display_access_records,name='display_access_records'),
+    path('select/',views.select,name='select'),
+    path('delete_topic/',views.delete_topic,name='delete_topic'),
+    path('multi_select/',views.multi_select,name='multi_select'),
+    path('checkbox/',views.checkbox,name='checkbox'),
+
+
+
+]
+admin.site.site_header='My app'
+admin.site.site_title='Dhoni'
+admin.site.index_title='raina'
